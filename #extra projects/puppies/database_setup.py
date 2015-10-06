d2c7a5fbefa5ec2859d715b6172f5c6452d639a6
razat249 <razat249@gmail.com>
2015-10-07 01:25:43 +0530

    Added an extra project puppies database.

---
+++ #extra projects/puppies/database_setup.py
@@ -0,0 +1,39 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String, Date
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy.orm import relationship
+
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Shelter(Base):
+	__tablename__ = 'shelter'
+	name = Column(String(80), nullable = False)
+	address = Column(String(200))
+	city = Column(String(80))
+	state = Column(String(80))
+	email = Column(String(40))
+	id = Column(Integer, primary_key = True)
+
+
+class Puppy(Base):
+	__tablename__ = 'puppy'
+	name = Column(String(80), nullable = False)
+	id = Column(Integer, primary_key = True)
+	date_of_birth = Column(Date)
+	breed = Column(String(250))
+	gender = Column(String(8))
+	weight = Column(Integer)
+	shelter_id = Column(Integer, ForeignKey('shelter.id'))
+	shelter = relationship(Shelter)
+
+
+############ insert at the end of file ################
+
+engine = create_engine('sqlite:///puppies.db')
+
+Base.metadata.create_all(engine)
